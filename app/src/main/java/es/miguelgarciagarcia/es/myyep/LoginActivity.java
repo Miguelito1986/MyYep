e9e754ec7578a6e7593150acba6ea789bc54152b
Miguelito1986 <migueel.angel.1986@gmail.com>
2016-01-11 16:48:59 +0100

    Primera Parte

---
+++ app/src/main/java/es/miguelgarciagarcia/es/myyep/LoginActivity.java
@@ -0,0 +1,25 @@
+package es.miguelgarciagarcia.es.myyep;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    protected TextView mSignupTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mSignupTextView=(TextView)findViewById(R.id.signuptextView);
+        mSignupTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
