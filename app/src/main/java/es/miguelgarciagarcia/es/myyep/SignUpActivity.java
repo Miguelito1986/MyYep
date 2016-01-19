2392b560636cb3ec55c11822dea67af19be0f2d2
Miguelito1986 <migueel.angel.1986@gmail.com>
2016-01-19 19:56:08 +0100

    Comprobacion ventana registro

--- app/src/main/java/es/miguelgarciagarcia/es/myyep/SignUpActivity.java
+++ app/src/main/java/es/miguelgarciagarcia/es/myyep/SignUpActivity.java
@@ -1,14 +1,70 @@
 package es.miguelgarciagarcia.es.myyep;
 
+import android.app.AlertDialog;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.parse.ParseUser;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText username;
+    EditText password;
+    EditText email;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         getSupportActionBar().hide();
+
+        username=(EditText)findViewById(R.id.usernamefield);
+        password=(EditText)findViewById(R.id.passwordfield);
+        email=(EditText)findViewById(R.id.editText3);
+
+
     }
+    public void onclickSignUp(View view){
+
+        String sName= String.valueOf(username.getText()).trim();
+        String sPassword=String.valueOf(password.getText()).trim();
+        String sEmail=String.valueOf(email.getText()).trim();
+        Resources res=getResources();
+        if(sName.isEmpty()){
+                AlertDialog.Builder builder=new AlertDialog.Builder(SignUpActivity.this);
+            String message =
+                    String.format(res.getString(R.string.empty_field_message), sName);
+                    builder.setMessage(message);
+                    builder.setTitle("username field");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setIcon(android.R.drawable.ic_dialog_alert);
+                    AlertDialog dialog=builder.create();
+                    dialog.show();
+        }else if(sPassword.isEmpty()){
+            AlertDialog.Builder builder=new AlertDialog.Builder(SignUpActivity.this);
+            String message =
+                    String.format(res.getString(R.string.empty_field_message),sPassword);
+                    builder.setMessage(message);
+                    builder.setTitle("password field");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setIcon(android.R.drawable.ic_dialog_alert);
+                    AlertDialog dialog=builder.create();
+                    dialog.show();
+        }else{
+            AlertDialog.Builder builder=new AlertDialog.Builder(SignUpActivity.this);
+            String message =
+                    String.format(res.getString(R.string.empty_field_message),sEmail);
+                    builder.setMessage(message);
+                    builder.setTitle("email field");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setIcon(android.R.drawable.ic_dialog_alert);
+                    AlertDialog dialog=builder.create();
+                    dialog.show();
+        }
+    }
+
 }
